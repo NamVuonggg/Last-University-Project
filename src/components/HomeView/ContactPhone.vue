eab826ef0304d84b2378f6b5ed754c67dc47d5a2
Nam Vuong <NamVuong453@gmail.com>
2024-06-16 22:57:47 +0900

    16/6/2024

--- src/components/HomeView/ContactPhone.vue
+++ src/components/HomeView/ContactPhone.vue
@@ -7,11 +7,14 @@ const router = useRouter();
 
 <template>
   <div>
-    <p>Hello</p>
+    <p class="txt font-bold text-xl text-center">070 8349 6***</p>
   </div>
 </template>
 
 
 <style scoped>
-
+@import url('https://fonts.googleapis.com/css2?family=Lilita+One&family=Playwrite+NL:wght@100..400&family=Shadows+Into+Light&display=swap');
+.txt{
+    font-family: "Shadows Into Light";
+}
 </style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,15 +1,19 @@
 <script setup>
-import { useRoute, useRouter } from 'vue-router';
+import { useRouter } from 'vue-router';
 
-const route = useRoute();
 const router = useRouter();
 
+const showPhone = () => {
+    return router.push(`/contact/phone`)
+}
 </script>
 
 <template>
-  <div class="h-5rem"></div>
+
+  <div class="h-10rem"></div>
   <p class="txt text-center font-bold text-8xl">MY PHONE NUMBER:</p>
-  <p class="txt text-center font-bold text-xl"><img src="https://cdn-icons-png.flaticon.com/128/10048/10048325.png">070 8349 60** </p>
+  <p class="txt text-center font-bold text-xl"><img src="https://cdn-icons-png.flaticon.com/128/10048/10048325.png"><Button @click="showPhone" class="bg-black-alpha-90 border-none hover:bg-red-600"> Show phone number</Button></p>
+  <RouterView />
 </template>
 
 
